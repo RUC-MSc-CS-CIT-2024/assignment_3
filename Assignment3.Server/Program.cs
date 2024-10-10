2f4a56245c43782d57e71f6b824722bcdaa23a54
Sofus Hilfling <sofus@techchapter.com>
2024-10-10 14:27:36 +0200

    feat: add validation logic and request handling

--- Assignment3.Server/Program.cs
+++ Assignment3.Server/Program.cs
@@ -1,9 +1,10 @@
 ﻿
 using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using Assignment3.Server;
 
 TcpListener listener = new TcpListener(IPAddress.Loopback, 5000);
 listener.Start();
@@ -19,13 +20,13 @@ async void HandleConnection(TcpClient client) {
     try {
         Request req = await client.ReadRequestAsync();
 
-        Response resp = await HandleRequestAsync(req);
+        Response resp = HandleRequest(req);
 
         await client.SendResponseAsync(resp);
 
     } catch (Exception e) {
         if (e.Message.Contains("An existing connection was forcibly closed by the remote host.."))
-            Console.WriteLine("Client disconnected");
+            Console.WriteLine($"Client '{client.Client.RemoteEndPoint}' disconnected");
         else {
             Console.WriteLine($"error: {e.Message}");
             await client.SendResponseAsync(new Response(e.Message));
@@ -36,12 +37,104 @@ async void HandleConnection(TcpClient client) {
     }
 }
 
-async Task<Response> HandleRequestAsync(Request request) {
-    if(request.Method == "echo") {
-        return new Response($"{(int)StatusCode.Ok} {StatusCode.Ok}", request.Body);
+Response HandleRequest(Request request) {
+    EnsureValidRequest(request);
+
+    if(request.Method == "echo")
+        return new Response($"1 Ok", request.Body);
+
+    string pathPattern = @"^\/api\/categories(\/(?<CategoryId>\d+))?$";
+    Match match = Regex.Match(request.Path, pathPattern);
+
+    if (!match.Success)
+        return ResponseFactory.CreateResponse(StatusCode.BadRequest);
+
+    Group catIdGroup = match.Groups["CategoryId"];
+    if (request.IsIdOnPathRequired() && !catIdGroup.Success)
+        return ResponseFactory.CreateResponse(StatusCode.BadRequest);
+    else if (request.IsIdOnPathNotAllowed() && catIdGroup.Success)
+        return ResponseFactory.CreateResponse(StatusCode.BadRequest);
+
+    Category? body = request.IsBodyRequired()
+        ? JsonSerializer.Deserialize<Category>(request.Body!, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase })
+        : null;
+    
+    int? categoryId = 
+        catIdGroup.Success 
+        ? int.Parse(catIdGroup.Value) 
+        : null;
+
+    switch (request.Method) {
+        case "read":
+            if (categoryId == null)
+                return ResponseFactory.CreateResponse(StatusCode.Ok, CategoryApi.GetCategories());
+            
+            Category? cat = CategoryApi.GetCategory(categoryId!.Value);
+            return cat == null
+                ? ResponseFactory.CreateResponse(StatusCode.NotFound)
+                : ResponseFactory.CreateResponse(StatusCode.Ok, cat);
+        case "create":
+            Category newCat = CategoryApi.CreateCategory(body!);
+            return ResponseFactory.CreateResponse(StatusCode.Created, newCat);
+        case "update":
+            return CategoryApi.UpdateCategory(categoryId!.Value, body!) 
+                ? ResponseFactory.CreateResponse(StatusCode.Updated)
+                : ResponseFactory.CreateResponse(StatusCode.NotFound);
+        case "delete":
+            return CategoryApi.DeleteCategory(categoryId!.Value) 
+                ? ResponseFactory.CreateResponse(StatusCode.Ok)
+                : ResponseFactory.CreateResponse(StatusCode.NotFound);
+        default:
+            return ResponseFactory.CreateResponse(StatusCode.BadRequest);
     }
-    return new("");
 }
+
+void EnsureValidRequest(Request request) {
+    string errorMsg = "";
+    if (string.IsNullOrEmpty(request.Method))
+        errorMsg += "Missing method ";
+    else if (!request.IsValidMethod())
+        errorMsg += "Illegal method";
+    if (request.Method != "echo" && string.IsNullOrEmpty(request.Path))
+        errorMsg += "Missing resource ";
+
+    if (string.IsNullOrEmpty(request.Date))
+            errorMsg += "Missing date ";
+    else if (!int.TryParse(request.Date, out int date))
+        errorMsg += "Illegal date ";
+    else if (date < 0 || date > DateTimeOffset.Now.ToUnixTimeSeconds())
+        errorMsg += "Illegal date ";
+
+    if (request.IsBodyRequired() && string.IsNullOrEmpty(request.Body))
+        errorMsg += "Missing body ";
+    else if (request.Method == "update" && !IsValidJson(request.Body))
+        errorMsg += "Illegal body ";
+
+    if (errorMsg != "")
+        throw new Exception(errorMsg.TrimEnd());
+    
+    Console.WriteLine("info: Valid request");
+}
+
+bool IsValidJson(string? strInput)
+    {
+        if (string.IsNullOrWhiteSpace(strInput)) return false;
+        strInput = strInput.Trim();
+        if ((strInput.StartsWith("{") && strInput.EndsWith("}")) // For object
+            || (strInput.StartsWith("[") && strInput.EndsWith("]")))   // For array
+        {
+            try
+            {
+                JsonDocument.Parse(strInput);
+                return true;
+            }
+            catch (JsonException) // Not valid JSON
+            {
+                return false;
+            }
+        }
+        return false;
+    }
 
 static class Util {
     public static async Task SendResponseAsync(this TcpClient client, Response response)
@@ -72,65 +165,13 @@ static class Util {
         // string responseData = await reader.ReadToEndAsync();
         Request? result = JsonSerializer.Deserialize<Request>(requestData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 
-        string errorMsg = "";
         if (result == null)
             throw new Exception("Failed to deserialize request");
-        if (string.IsNullOrEmpty(result.Method))
-            errorMsg += "Missing method ";
-        else if (!result.IsValidMethod())
-            throw new Exception("Illegal method");
-
-        if (result.Method != "echo" && string.IsNullOrEmpty(result.Path))
-            errorMsg += "Missing resource ";
-
-        if (string.IsNullOrEmpty(result.Date))
-            errorMsg += "Missing date ";
-        else if (!int.TryParse(result.Date, out int date))
-            errorMsg += "Illegal date ";
-        else if (date < 0 || date > DateTimeOffset.Now.ToUnixTimeSeconds())
-            errorMsg += "Illegal date ";
-
-        if (result.IsBodyRequired() && string.IsNullOrEmpty(result.Body))
-            errorMsg += "Missing body ";
-        else if (result.Method == "update" && !IsValidJson(result.Body))
-            errorMsg += "Illegal body ";
-            
-        if (errorMsg != "")
-            throw new Exception(errorMsg.TrimEnd());
         
         return result;
     }
-
-    
-    static bool IsValidJson(string? strInput)
-    {
-        if (string.IsNullOrWhiteSpace(strInput)) return false;
-        strInput = strInput.Trim();
-        if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || // For object
-            (strInput.StartsWith("[") && strInput.EndsWith("]")))   // For array
-        {
-            try
-            {
-                JsonDocument.Parse(strInput);
-                return true;
-            }
-            catch (JsonException) // Not valid JSON
-            {
-                return false;
-            }
-        }
-        return false;
-    }
 }
 
-enum StatusCode {
-    Ok = 1,
-    Created = 2,
-    Updated = 3,
-    BadRequest = 4,
-    NotFound = 5,
-    Error = 6
-}
 record Request(string Method, string Path, string Date, string? Body = null) {
     public bool IsValidMethod() 
         => Method == "create" 
@@ -143,5 +184,40 @@ record Request(string Method, string Path, string Date, string? Body = null) {
         => Method == "create" 
         || Method == "update" 
         || Method == "echo";
+    
+    public bool IsIdOnPathRequired()
+        => Method == "update"
+        || Method == "delete";
+    
+    public bool IsIdOnPathNotAllowed()
+        => Method == "create";
 }
-record Response(string Status, string? Body = null);
+public record Response(string Status, string? Body = null);
+public enum StatusCode {
+    Ok = 1,
+    Created = 2,
+    Updated = 3,
+    BadRequest = 4,
+    NotFound = 5,
+    Error = 6
+}
+public static class ResponseFactory {
+
+    public static Response CreateResponse(StatusCode statusCode) {
+        string status = statusCode switch {
+            StatusCode.Ok => "1 Ok",
+            StatusCode.Created => "2 Created",
+            StatusCode.Updated => "3 Updated",
+            StatusCode.BadRequest => "4 Bad Request",
+            StatusCode.NotFound => "5 Not Found",
+            StatusCode.Error => "6 Error",
+            _ => throw new NotImplementedException()
+        };
+        return new Response(status);
+    }
+    public static Response CreateResponse<T>(StatusCode statusCode, T body) {
+        
+        return CreateResponse(statusCode) 
+            with { Body = JsonSerializer.Serialize(body, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }) };
+    }
+}
